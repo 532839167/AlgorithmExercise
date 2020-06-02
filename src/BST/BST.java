1732203e4bdbf8fcddeb4b1c183a36345441a1f2
jin_song <jinsong99598@gmail.com>
2020-06-01 23:18:33 -0400

    BST add

--- src/BST/BST.java
+++ src/BST/BST.java
@@ -28,4 +28,29 @@ public class BST<E extends Comparable<E>> {
     public boolean isEmplty() {
         return size == 0;
     }
+
+    public void add(E e) {
+
+        root = add(root, e);
+    }
+
+    // 向以node为根的二分搜索树中插入元素e，递归，返回插入新节点后 BST的根
+    private Node add(Node node, E e) {
+
+        // Base Case
+        if (node == null) {
+            size ++;
+            return new Node(e);
+        }
+
+        // recursion
+        if (e.compareTo(node.e) < 0) {
+            node.left = add(node.left, e);
+        } else if (e.compareTo(node.e) > 0){
+            node.right = add(node.right, e);
+        }
+
+        return node;
+
+    }
 }
